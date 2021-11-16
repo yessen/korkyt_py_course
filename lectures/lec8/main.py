14fe7b8184756dba52545dac0ab21cdc894d6b42
yessen <yessen@mail.ru>
2021-11-16 10:45:06 +0600

    added seminar tasks

---
+++ lectures/lec8/main.py
@@ -0,0 +1,15 @@
+### Main program 
+
+# Import  modules
+
+
+# Read data file
+
+
+# Parse file and create a dictionary
+
+
+# Infinite loop for queries
+
+
+# Finish the program
---
+++ lectures/lec8/text_parser.py
@@ -0,0 +1,53 @@
+####################################
+### This modules works with file ###
+####################################
+
+# Data loader function
+# - input:  filename
+# - output: list of lines
+
+
+# Word count function
+# - input:  list of lines
+# - output: dictionary of words and counts
+
+
+# Get the number of words
+# - input:  dictionary of words and counts
+# - output: the number of words
+
+
+# Get the count of all words
+# - input:  dictionary of words and counts
+# - output: sum of all counts in dictionary
+
+
+# Get the average count of words
+# - input:  dictionary of words and counts
+# - output: average count of words
+
+
+# Get the word's count function
+# - input: 
+#     - word to query
+#     - dictionary of words and counts
+# - output: word count
+ 
+
+# Check if a word is in the dictionary
+# - input: 
+#     - word to check
+#     - dictionary of words and counts      
+# - output: True if word is in dictionary, False - otherwise
+
+
+# Save dictionary to the file
+# - input:  
+#     - filename
+#     - dictionary of words and counts      
+# - output: message if the file is saved or not
+
+
+# Test all functions here
+
+
