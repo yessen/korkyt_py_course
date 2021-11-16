601b640cd287211e6c2b296f767d79642a73162a
yessen <yessen@mail.ru>
2021-11-16 13:22:33 +0600

    project skeleton added

--- lectures/lec8/main.py
+++ lectures/lec8/main.py
@@ -1,15 +1,44 @@
 ### Main program 
 
 # Import  modules
+import text_parser as tp
 
+print('Entered main program')
+
+# Enter file name to read
+fname = input('Enter file name to read: ')
+print(fname)
 
 # Read data file
-
+lines = tp.data_loader(fname)
+print(len(lines))
 
 # Parse file and create a dictionary
-
+dict1 = tp.get_word_counts(lines)
+print(len(dict1))
 
 # Infinite loop for queries
-
+while True:
+  tsk = int(input('Select task:'))
+  if tsk == 1:
+     # 1-shi funct wakyru
+  elif tsk == 2:
+     # 2-shi funct wakyru
+  elif tsk == 3:
+     # 3-shi funct wakyru
+  elif tsk == 4:
+     # 4-shi funct wakyru
+  elif tsk == 5:
+     # 5-shi funct wakyru
+  elif tsk == 6:
+     # 6-shi funct wakyru
+  elif tsk == 7:
+     # 7-shi funct wakyru
+  elif tsk == 8:
+     # 8-shi funct wakyru
+  else: 
+     # programmadan shygu
+     exit()
+   
 
 # Finish the program
--- lectures/lec8/text_parser.py
+++ lectures/lec8/text_parser.py
@@ -2,52 +2,89 @@
 ### This modules works with file ###
 ####################################
 
+# Obshiy
 # Data loader function
 # - input:  filename
 # - output: list of lines
+def data_loader(filename):
+    print('data_loader function')
+    try:
+       fin = open(filename)
+       lines = fin.readlines()
+       fin.close()
+    except:   
+       print('File ashylmadi')
+       return []
 
+    return lines
 
-# Word count function
+# Galymzhan
+# 1. Word count function
 # - input:  list of lines
 # - output: dictionary of words and counts
+def get_word_counts(lines):
+    pass 
 
-
-# Get the number of words
+# Damir
+# 2. Get the number of words
 # - input:  dictionary of words and counts
 # - output: the number of words
+def get_number_word(dict1):
+    pass
 
 
-# Get the count of all words
-# - input:  dictionary of words and counts
+# Erkebulan
+# 3. Get the sum of counts of all words
+# - input:  mydict - dictionary of words and counts - { word: count }
 # - output: sum of all counts in dictionary
+def get_sum_counts(mydict):
+    pass
 
-
-# Get the average count of words
+# Aiya
+# 4. Get the average count of words
 # - input:  dictionary of words and counts
 # - output: average count of words
+def count_average(dict1):
+    pass
 
-
-# Get the word's count function
+# Sultan
+# 5. Get the word's count function
 # - input: 
 #     - word to query
 #     - dictionary of words and counts
 # - output: word count
+def get_count(query, dict1):   
+    pass
  
-
-# Check if a word is in the dictionary
+# Sunkar + Olzhas
+# 6. Check if a word is in the dictionary
 # - input: 
 #     - word to check
 #     - dictionary of words and counts      
 # - output: True if word is in dictionary, False - otherwise
 
 
-# Save dictionary to the file
+# Timur
+# 7. Save dictionary to the file
 # - input:  
 #     - filename
 #     - dictionary of words and counts      
 # - output: message if the file is saved or not
+def save_dic_to_file(dic, filename):
+    pass
+
+
+# Aibek
+# 8. Get maximum length word
+# - input: dictionary - aynimalyga at beru kerek
+# - output: maximum length word   
+def find_longest_word(dict1): 
+    pass 
 
 
 # Test all functions here
-
-
+if __name__ == "__main__":
+   print("Testing module ...")
+   
+   content = data_loader('brown_short.txt')
+   print(len(content))
